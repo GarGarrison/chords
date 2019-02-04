1c49b0cbdd44beb670578b4d07cb8a3a106e284c
garrison <garrisongar@gmail.com>
2019-02-04 16:44:58 +0300

    add metrics routes

--- dbloader.py
+++ dbloader.py
@@ -49,48 +49,44 @@ def read_old():
             print(data)
             raise
 
+def read_file(fname):
+    with open(fname, 'r') as f:
+        return json.load(f)
+
+def read_chords(func):
+    def wrapper():
+            i = 0
+
+            # to test parsing
+            for dirname in os.listdir(IN_DIR):
+                for fname in os.listdir(os.path.join(IN_DIR, dirname)):
+                    fpath = os.path.join(os.path.join(IN_DIR, dirname), fname)
+                    try:
+                        jsondata = read_file(fpath)
+                        func(jsondata, dirname, fname, fpath)
+                    except Exception as e:
+                        print(fpath)
+                        raise
+                    i += 1
+                    if i % 1000 == 0:
+                        print('Files processed: %s' % i)
+    return wrapper
+
+@read_chords
+def insert_song(jsondata, dirname, fname, fpath):
+    r = db.insert_song(jsondata)
+    if r:
+        newdir = os.path.join(OUT_DIR, dirname)
+        olddir = os.path.join(IN_DIR, dirname)
+        newfile = os.path.join(newdir, fname)
+        if not os.path.exists(newdir): os.makedirs(newdir)
+        os.rename(fpath, newfile)
+        if len(os.listdir(olddir)) == 0: os.rmdir(olddir)
 
 
-
-
-# def read_file(fname):
-#     with open(fname, 'r') as f:
-#         return json.load(f)
-
-# def read_chords(func):
-#     def wrapper():
-#             i = 0
-
-#             # to test parsing
-#             for dirname in os.listdir(IN_DIR):
-#                 for fname in os.listdir(os.path.join(IN_DIR, dirname)):
-#                     fpath = os.path.join(os.path.join(IN_DIR, dirname), fname)
-#                     try:
-#                         jsondata = read_file(fpath)
-#                         func(db, jsondata, dirname, fname, fpath)
-#                     except Exception as e:
-#                         print(fpath)
-#                         raise
-#                     i += 1
-#                     if i % 1000 == 0:
-#                         print('Files processed: %s' % i)
-#     return wrapper
-
-# @read_chords
-# def insert_song(db, jsondata, dirname, fname, fpath):
-#     r = db.insert_song(jsondata)
-#     if r:
-#         newdir = os.path.join(OUT_DIR, dirname)
-#         olddir = os.path.join(IN_DIR, dirname)
-#         newfile = os.path.join(newdir, fname)
-#         if not os.path.exists(newdir): os.makedirs(newdir)
-#         os.rename(fpath, newfile)
-#         if len(os.listdir(olddir)) == 0: os.rmdir(olddir)
-
-
-# @read_chords
-# def insert_artist(db, jsondata, *args):
-#     db.insert_artist(jsondata)
+@read_chords
+def insert_artist(jsondata, *args):
+    db.insert_artist(jsondata)
 
 if __name__ == '__main__':
     # insert_artist()
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/google08297e6daadffa99.html', function(){
+    return File::get(public_path() . '/google08297e6daadffa99.html');
+});
+Route::get('/yandex_3a8bee90a3ca9a08.html', function(){
+    return File::get(public_path() . '/yandex_3a8bee90a3ca9a08.html');
+});
 Route::get('/', 'MainController@index');
 Route::get('/news', 'MainController@news');
 Route::get('/popular', 'MainController@popular');
